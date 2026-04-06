fee8d6d5848e13c5a2643314dee87f833d08c067
jratul <jratul@github.com>
2026-04-06 19:38:29 +0900

    feat: set up favicon, character asset, and Pretendard font

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "pretendard/dist/web/variable/pretendardvariable-dynamic-subset.css";
 import "katex/dist/katex.min.css";
-import "@fontsource/noto-sans-kr/400.css";
-import "@fontsource/noto-sans-kr/700.css";
 import "./index.css";
 import App from "./App";
 
