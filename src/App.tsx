3c026ed60fc6671b23ba847dea16e5b8ee41111f
jratul <jratul@nate.com>
2026-04-12 23:04:49 +0900

    feat: scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { Layout } from "@components/layout/Layout";
 import { HomePage } from "@pages/HomePage";
 import { SubjectPage } from "@pages/SubjectPage";
 import { ArticlePage } from "@pages/ArticlePage";
 import { SearchPage } from "@pages/SearchPage";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/*"
-        element={
-          <Layout>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/search" element={<SearchPage />} />
-              <Route path="/:subject" element={<SubjectPage />} />
-              <Route path="*" element={<ArticlePage />} />
-            </Routes>
-          </Layout>
-        }
-      />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route
+          path="/*"
+          element={
+            <Layout>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/search" element={<SearchPage />} />
+                <Route path="/:subject" element={<SubjectPage />} />
+                <Route path="*" element={<ArticlePage />} />
+              </Routes>
+            </Layout>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
